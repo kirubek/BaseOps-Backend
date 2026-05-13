27d95c60ca53dad3ffb1368f9b53c22a6c06d8f7
kirubek <65424303+kirubek@users.noreply.github.com>
2026-05-12 23:37:42 -0700

    Add missing Shop entity

---
+++ AviationMaintenance/Domain/Entities/Shop.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace AviationMaintenance.Domain.Entities
+{
+    /// <summary>
+    /// Represents a maintenance shop facility
+    /// </summary>
+    public class Shop : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // Navigation properties
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+        public virtual ICollection<Bulletin> Bulletins { get; set; } = new List<Bulletin>();
+    }
+}
