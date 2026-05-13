29eb1713e37fe966f599dddc6ea59c18525796a8
kirubek <65424303+kirubek@users.noreply.github.com>
2026-05-12 23:37:20 -0700

    Add missing Hangar entity

---
+++ AviationMaintenance/Domain/Entities/Hangar.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace AviationMaintenance.Domain.Entities
+{
+    /// <summary>
+    /// Represents a maintenance hangar facility
+    /// </summary>
+    public class Hangar : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // Navigation properties
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+        public virtual ICollection<Bulletin> Bulletins { get; set; } = new List<Bulletin>();
+    }
+}
